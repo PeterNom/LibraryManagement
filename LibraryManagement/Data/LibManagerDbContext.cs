fefff6b07b54063861f19bad4d9876419b1f7001
Panagiotis Nomikos <102253139+PeterNom@users.noreply.github.com>
2024-03-09 14:51:31 +0200

    Added postgreSQL database.

---
+++ LibraryManagement/Data/LibManagerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Data
+{
+    public class LibManagerDbContext: DbContext
+    {
+        public LibManagerDbContext(DbContextOptions<LibManagerDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -1,4 +1,10 @@
+using LibraryManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<LibManagerDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 

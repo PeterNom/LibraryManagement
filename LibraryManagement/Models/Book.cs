d78c3e3b1ab294aaa92aa4d3e441a8c27e1990cf
Panagiotis Nomikos <102253139+PeterNom@users.noreply.github.com>
2024-03-09 13:50:36 +0200

    Add project files.

---
+++ LibraryManagement/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagement.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty ;
+        public DateTime PublishedDate { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; } = null!;
+    }
+}
